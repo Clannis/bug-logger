4abd35cfba3e585a01fb8b0c5d2bad418c1dc430
clannis <dodds1990@gmail.com>
2021-03-26 18:49:25 -0500

    Format created date using momment component

--- src/components/LogItem.js
+++ src/components/LogItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 import Badge from 'react-bootstrap/Badge'
+import Moment from 'react-moment'
 
 const LogItem = ({ log : { _id, priority, text, user, created} }) => {
 
@@ -19,7 +20,7 @@ const LogItem = ({ log : { _id, priority, text, user, created} }) => {
             <td><Badge variant={setVariant()} className='p-2'>{priority.charAt(0).toUpperCase() + priority.slice(1)}</Badge></td>
             <td>{text}</td>
             <td>{user}</td>
-            <td>{created}</td>
+            <td><Moment format='MMMM Do YYYY, h:mm:ss a'>{new Date(created)}</Moment></td>
             <td><Button variant='danger' size="sm">x</Button></td>
         </tr>
     )

4a419447eb45d645e8052e891219afefa75e2105
clannis <dodds1990@gmail.com>
2021-03-26 19:25:47 -0500

    Create alert function controlled by state to show alert on new log submitted and add validation to show different alert if not valid

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import Table from 'react-bootstrap/Table'
+import Alert from 'react-bootstrap/Alert'
 import AddLogItem from './AddLogItem'
 import LogItem from './LogItem'
 
 const App = () => {
+	const [alert, setAlert] = useState({
+		show: false,
+		message: '',
+		variant: 'success'
+	})
 	const [logs, setLogs] =useState([
 		{
 			_id: 1,
@@ -30,14 +36,36 @@ const App = () => {
 	])
 
 	function addItem(item) {
+		if(item.text === '' || item.user === '' || item.priority === '') {
+			showAlert('Please Enter All Fields', 'danger')
+			return false
+		}
+
 		item._id = Math.floor(Math.random() * 90000) + 10000
 		item.created = new Date().toString()
 		setLogs([...logs, item])
+		showAlert('Log Added')
+	}
+
+	function showAlert(message, variant = 'success', seconds = 3000) {
+		setAlert({
+			show: true,
+			message,
+			variant
+		})
+		setTimeout(() => {
+			setAlert({
+				show: false,
+				message: '',
+				variant: ''
+			})
+		}, seconds)
 	}
 
 	return (
 		<Container>
 			<AddLogItem addItem={addItem}/>
+			{ alert.show && <Alert variant={alert.variant}>{alert.message}</Alert>}
 			<Table>
 				<thead>
 					<tr>

001584cfff741763c424b678de85f3e5cad5b447
clannis <dodds1990@gmail.com>
2021-03-26 21:03:18 -0500

    Create log model db schema and import into main

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const url = require('url')
 const { app, BrowserWindow } = require('electron')
+const Log = require('./models/log')
 const connectDB = require('./config/db')
 
 // Connect to database

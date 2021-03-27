c7910f3ad7fb507232ec906af887631e1d47d55b
clannis <dodds1990@gmail.com>
2021-03-26 21:23:42 -0500

    Set DB to buglogger instead of test

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    const mongoAltasUri = 'mongodb+srv://Clannis:clannis@clanniscluster.tmut2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
     try {
         const conn = await mongoose.connect(
-            'mongodb+srv://Clannis:clannis@clanniscluster.tmut2.mongodb.net/test?retryWrites=true&w=majority',
+            'mongodb+srv://Clannis:clannis@clanniscluster.tmut2.mongodb.net/buglogger?retryWrites=true&w=majority',
             {
                 useNewUrlParser: true,
                 useCreateIndex: true,
